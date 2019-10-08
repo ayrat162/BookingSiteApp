f3f5c89218a8b0d48081ceb05bd8b3c38265dba8
Ayrat Musin <musinayrat@gmail.com>
2019-10-08 06:30:24 +0300

    2019.10.08 initial commit

---
+++ BookingDAL/BookingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookingShared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingDAL
+{
+    public class BookingDbContext : DbContext
+    {
+        public BookingDbContext(DbContextOptions<BookingDbContext> options)
+            : base(options) { }
+
+        public DbSet<HotelModel> Hotels { get; set; }
+    }
+}
---
+++ BookingSite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BookingSite.Models;
+using BookingDAL;
+using Microsoft.EntityFrameworkCore;
+using BookingShared.Models;
+
+namespace BookingSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DbContext _dbContext;
+        public HomeController(BookingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _dbContext.Set<HotelModel>().ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
