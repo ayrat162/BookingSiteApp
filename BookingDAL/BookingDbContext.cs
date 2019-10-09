c4b3a12fe1c598d090dc13a52f5ca4688579c4ac
Ayrat Musin <musinayrat@gmail.com>
2019-10-09 06:34:56 +0300

    added unique keys

--- BookingDAL/BookingDbContext.cs
+++ BookingDAL/BookingDbContext.cs
@@ -8,6 +8,14 @@ namespace BookingDAL
         public BookingDbContext(DbContextOptions<BookingDbContext> options)
             : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelDetailsModel>()
+                .HasIndex(e => e.HotelModelId).IsUnique(true);
+            modelBuilder.Entity<RoomModel>()
+                .HasIndex(e => new { e.HotelModelId, e.RoomNumber }).IsUnique(true);
+        }
+
         public DbSet<HotelModel> Hotels { get; set; }
         public DbSet<HotelDetailsModel> Details { get; set; }
         public DbSet<RoomModel> Rooms { get; set; }
