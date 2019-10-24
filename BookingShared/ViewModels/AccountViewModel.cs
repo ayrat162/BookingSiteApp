f31a6f4281d0a05638e5e4ba0096814ee48ced6f
Ayrat Musin <musinayrat@gmail.com>
2019-10-24 21:41:51 +0300

    started implementing account page added populate for rooms added real rooms to hotel view

---
+++ BookingShared/ViewModels/AccountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingShared.ViewModels
+{
+    // TODO: Add to Account-Index View
+    public class AccountViewModel
+    {
+    }
+}
