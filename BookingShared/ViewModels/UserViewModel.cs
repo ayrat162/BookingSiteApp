5110ed5282b35a8bdae2910b9c0061e0f8a6147e
Ayrat Musin <musinayrat@gmail.com>
2019-11-02 09:42:44 +0300

    created UserInfo view

---
+++ BookingShared/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BookingShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingShared.ViewModels
+{
+    public class UserViewModel
+    {
+        public AppUser user { get; set; }
+        public List<string> roles { get; set; }
+    }
+}
--- BookingSite/Controllers/AdminController.cs
+++ BookingSite/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using BookingShared.Interfaces;
 using BookingShared.Models;
+using BookingShared.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +31,36 @@ namespace BookingSite.Controllers
             var user = await _userManager.GetUserAsync(HttpContext.User);
             var roles = await _userManager.GetRolesAsync(user);
             return View(roles);
+        }
+
+        public async Task<IActionResult> Users()
+        {
+            var usersWithRoles = new List<UserViewModel>();
+            var users = _userManager.Users.ToList();
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                usersWithRoles.Add(new UserViewModel
+                {
+                    user = user,
+                    roles = roles.ToList()
+                });
+
+            }
+            return View(usersWithRoles);
+        }
+
+
+        public async Task<IActionResult> UserInfo(int id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            var roles = await _userManager.GetRolesAsync(user);
+            var userInfo = new UserViewModel
+            {
+                user = user,
+                roles = roles.ToList()
+            };
+            return View(userInfo);
         }
 
         public async Task<IActionResult> BecomeAdmin()
